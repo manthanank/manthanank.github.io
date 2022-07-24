1d444fbd773ccf78a2fec98009ad35f0d5034df6
Manthan Ankolekar <manthan.ank46@gmail.com>
2022-07-24 15:04:46 +0530

    changes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./components/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./components/home.module').then(m => m.HomeModule) },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
