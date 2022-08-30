31a943a5101def757a2bbe53e2ce54d79a11941b
Manthan Ankolekar <manthan.ank46@gmail.com>
2022-08-30 09:07:46 +0530

    changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NgOptimizedImage } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
