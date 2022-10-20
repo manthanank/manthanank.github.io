cae1af70fde7ac19439f8c9bf2fb3c85227765fa
Manthan Ankolekar <manthan.ank46@gmail.com>
2022-10-20 19:08:45 +0530

    changes made

---
+++ src/app/services/projects.ts
@@ -0,0 +1,5 @@
+export interface Projects {
+    id: string;
+    name: string;
+    desc: string;
+}
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Project } from '../pages/projects/projects';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
   constructor(private fs: Firestore) {
+  }
+
+  getProjects() {
   }
 }
