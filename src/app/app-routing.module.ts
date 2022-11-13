0b14b3f1321be8961ccc85190a2ce25dffd935a8
Manthan <manthan.ank46@gmail.com>
2022-11-13 15:28:48 +0530

    changes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'profiles', loadComponent: () => import('./pages/profiles/profiles.component').then(m => m.ProfilesComponent) },
   { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
   { path: 'projects', loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent) }
 ];
