9dd201bf0b72e4f061ade41631602632198c149e
Manthan <manthan.ank46@gmail.com>
2022-11-19 15:53:02 +0530

    changes made

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from '../app-routing.module';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, AppRoutingModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
