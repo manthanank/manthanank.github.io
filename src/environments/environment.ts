727a113042cc3a572be4b07d28eeaa42821e4de3
Manthan Ankolekar <manthan.ank@gmail.com>
2022-12-12 19:11:38 +0530

    changes made

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    production: false
-  };
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDlWzvBtERyiJqL4hlFsCmXG83j3WRLAcM",
+    authDomain: "manthanank.firebaseapp.com",
+    projectId: "manthanank",
+    storageBucket: "manthanank.appspot.com",
+    messagingSenderId: "155323854842",
+    appId: "1:155323854842:web:8dd40edf91f9839ca7be43"
+  },
+};
