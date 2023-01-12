4ea15bcf5e3d264b76bb2b57860f06a932c24a05
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-12 23:53:25 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
   { path: 'projects', loadComponent: () => import('./projects/projects.component').then(m => m.ProjectsComponent) },
-  { path: 'blogs', loadComponent: () => import('./blogs/blogs.component').then(m => m.BlogsComponent) },
-  { path: 'contact', loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent) },
-  { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent) }
+  { path: 'blogs', loadComponent: () => import('./blogs/blogs.component').then(m => m.BlogsComponent) }
 ];
 
 @NgModule({
