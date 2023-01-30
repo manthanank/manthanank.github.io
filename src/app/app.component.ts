1d8dc8bfe4d22d4cfff6c4329604a7f23cd3868c
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-31 00:46:32 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, OnInit} from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 import { ToastrService } from 'ngx-toastr';
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WipComponent } from './wip/wip.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterModule,
+    HttpClientModule,
+    LoadingBarRouterModule,
+    NgOptimizedImage,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    WipComponent,
+    UiSwitchModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'portfolio';
@@ -16,7 +37,7 @@ export class AppComponent implements OnInit {
     // });
   }
   isDark = false;
-  
+
   toggle() {
     this.isDark
       ? document.body.classList.remove('dark')
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule)
+  ],
+});
