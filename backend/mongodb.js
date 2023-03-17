48964e2fbff338216dd9b99ad0ed62ab1b80c60c
Manthan Ankolekar <manthan.ank@gmail.com>
2023-03-18 00:15:35 +0530

    db name changed

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -6,7 +6,7 @@ mongoose
     .connect(
         "mongodb+srv://manthanank:" +
         process.env.MONGO_ATLAS_PW +
-        "@cluster0.re3ha3x.mongodb.net/profile"
+        "@cluster0.re3ha3x.mongodb.net/manthanank-github-io"
     )
     .then(() => {
         console.log("Connected to MongoDB database!");
