f3a0acba5ec0e1e02e8b336a4d7fdcc4b6cb0baf
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-09-03 18:15:44 +0530

    feat: Update resume page with education and contact sections

--- js/data.js
+++ js/data.js
@@ -1,30 +1,10 @@
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
-    const {
-      fullName,
-      shortName,
-      about,
-      resumeUrl,
-      contact: { email, phone },
-      title,
-      skills,
-      socialLinks,
-      career,
-      education,
-      address,
-      projects,
-    } = data;
+    const { resumeUrl, skills, socialLinks, career, education } = data;
 
-    document.getElementsByClassName("full-name")[0].textContent = fullName;
-    document.getElementsByClassName("short-name")[0].textContent = shortName;
-    document.getElementsByClassName("about")[0].textContent = about;
     document.getElementById("resume").href = resumeUrl;
-    document.getElementById("email").textContent = email;
-    document.getElementById("email").href = `mailto:${email}`;
-    document.getElementById("phone").textContent = phone;
-    document.getElementById("phone").href = `tel:${phone}`;
-    document.getElementsByClassName("title")[0].textContent = title;
+
     skills.forEach((skill) => {
       document.querySelector(".skill-bars-fat").innerHTML += `
         <li>
@@ -33,6 +13,7 @@ fetch("data.json")
         </li>
       `;
     });
+
     socialLinks.forEach((link) => {
       document.querySelector(".s-hero__content-social").innerHTML += `
         <a href="${link.url}" aria-label="${link.name} link"><i class="fab fa-${link.icon}" aria-hidden="true"></i></a>
@@ -43,10 +24,6 @@ fetch("data.json")
       </li>
     `;
     });
-    document.getElementById("state").textContent = address.state;
-    document.getElementById(
-      "address"
-    ).textContent = `${address.state}, ${address.country}`;
 
     career.forEach((job) => {
       // console.log(job);
@@ -96,41 +73,5 @@ fetch("data.json")
       </div>
       `;
     });
-
-    projects.forEach((project) => {
-      // console.log(project);
-    });
-    
-    document.querySelector("#contact .column.large-12").innerHTML = `
-      <h3 class="section-header-allcaps">Contact</h3>
-      <div class="row s-contact__content">
-        <div class="column large-7 medium-12">
-          <h4 class="huge-text">You can contact me through this.👉</h4>
-        </div>
-        <div class="column large-4 medium-12">
-          <div class="row contact-infos">
-            <div class="column large-12 medium-6 tab-12">
-              <div class="contact-block">
-                <h5 class="contact-block__header">Email</h5>
-                <p class="contact-block__content">
-                  <a class="mailtoui" href="mailto:${email}" aria-label="email to">${email}</a>
-                </p>
-              </div>
-            </div>
-            <div class="column large-12 medium-6 tab-12">
-              <div class="contact-block">
-                <h5 class="contact-block__header">Phone</h5>
-                <p class="contact-block__content">
-                  <a href="tel:${phone}" aria-label="contact number">${phone}</a>
-                </p>
-              </div>
-            </div>
-            <div class="column large-12">
-              <a href="mailto:${email}" class="mailtoui btn h-full-width" aria-label="email id">Let's Talk</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
   })
   .catch((error) => console.error("Error fetching JSON:", error));
